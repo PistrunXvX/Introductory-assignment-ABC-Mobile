9a4ef8f423ce93a04a32debe7c36c05476e3a547
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2021-03-12 22:44:29 +0300

    Done

--- js/script.js
+++ js/script.js
@@ -98,6 +98,78 @@ function createPage5(data){
     }
 
     questionText.textContent = 'Запись, которую Вы услышите, может шокировать людей с неокрепшей психикой. Вы готовы узнать, что ждет именно Вас?';
+    questionNumber.textContent = 'Вопрос 5-5';
+
+    let formPage4 = document.querySelector('.form-content__hidden');
+    let formPage5 = document.querySelector('.form-content__hidden_2');
+    let addPopup = document.querySelector('.add_popup');
+    formPage4.style.display = 'none';
+    formPage5.style.display = 'block';
+    addPopup.classList.add('bg_popup');
+    headerText.style.color = "#202024";
+}
+
+function createPageResult(data) {
+    formData.done = data;
+    let progress = document.querySelector('.progress-wrapper');
+    let resultPage = document.querySelector('.result-content');
+    let questionHeader = document.querySelector('.header-question');
+    let formContent = document.querySelector('.form-content');
+
+    function progressbar() {
+        let bar = document.querySelector('.progressbar-inside');
+        let percent = document.querySelector('.progressbar-text');
+        let width = 10;
+        let id = setInterval(frame, 50);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+            } else {
+                width++;
+                bar.style.width = width + '%';
+                percent.innerHTML = width * 1 + '%';
+            }
+        }
+    }
+
+    mainContent.style.display = 'none';
+    progress.style.display = 'block';
+
+    progressbar();
+
+    setTimeout(createPage, 4000);
+
+    function createPage() { 
+        mainContent.style.display = 'block';
+        progress.style.display = 'none';
+        questionHeader.style.display = 'none';
+        formContent.style.display = 'none';
+        questionNumber.style.display = 'none';
+        resultPage.style.display = 'block';
+
+        let nextDate = document.querySelector('.next-date');
+        let currentDate = new Date();
+        let day = currentDate.getDate();
+        nextDate.innerHTML = day + 1;
+    }
+
+}
+
+function showResult() {
+    let show1 = document.querySelector('.show-1');
+    let show2 = document.querySelector('.show-2');
+    let show3 = document.querySelector('.show-3');
+    let show4 = document.querySelector('.show-4');
+    let show5 = document.querySelector('.show-5');
+    let showList = document.querySelector('.result-show');
+
+    showList.style.display = 'block';
+
+    show1.innerHTML += formData.die + '';
+    show2.innerHTML += formData.comfort + '';
+    show3.innerHTML += formData.date.day + '.' + formData.date.month + '.' + formData.date.year;
+    show4.innerHTML += formData.sleepDiePeopel;
+    show5.innerHTML += formData.done;
 }
 
 
